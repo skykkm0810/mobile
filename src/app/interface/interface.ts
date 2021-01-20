d83ce471e75e5556d13f3dca5b4f0522d6895b22
skykkm0810 <skykkm0810@naver.com>
2021-01-20 11:29:19 +0900

    dateClick

--- src/app/interface/interface.ts
+++ src/app/interface/interface.ts
@@ -12,10 +12,10 @@ export interface Program {
     imgurl?:string;
 }
 export const PROGRAM : Program[] = [
-    {id:1, name:'댄스교실', time:'오전 11:00',class:'제 2교실', date:'2021-01-07',imgurl:''},
-    {id:2, name:'요리교실', time:'오후 02:00',class:'제 2교실', date:'2021-01-07',imgurl:''},
-    {id:3, name:'노래교실', time:'오전 11:00',class:'제 2교실', date:'2021-01-06',imgurl:''},
-    {id:4, name:'노래교실', time:'오후 02:00',class:'제 2교실', date:'2021-01-06',imgurl:''},
-    {id:5, name:'보컬교실', time:'오전 11:00',class:'제 1교실', date:'2021-01-05',imgurl:''},
-    {id:6, name:'문학교실', time:'오후 02:00',class:'제 1교실', date:'2021-01-05',imgurl:''},
+    {id:1, name:'댄스교실', time:'오전 11:00',class:'제 2교실', date:'2021-01-19',imgurl:''},
+    {id:2, name:'요리교실', time:'오후 02:00',class:'제 2교실', date:'2021-01-19',imgurl:''},
+    {id:3, name:'노래교실', time:'오전 11:00',class:'제 2교실', date:'2021-01-20',imgurl:''},
+    {id:4, name:'악기교실', time:'오후 02:00',class:'제 2교실', date:'2021-01-20',imgurl:''},
+    {id:5, name:'보컬교실', time:'오전 11:00',class:'제 1교실', date:'2021-01-21',imgurl:''},
+    {id:6, name:'문학교실', time:'오후 02:00',class:'제 1교실', date:'2021-01-21',imgurl:''},
 ]
--- src/app/page/program/program.component.ts
+++ src/app/page/program/program.component.ts
@@ -10,11 +10,17 @@ export class ProgramComponent implements OnInit {
   constructor() { }
   ngOnInit(): void {
     this.background()
+
     var year = (new Date).getFullYear();
     var month = (new Date).getMonth() + 1;
-    var date = (new Date).getDate();
+    var day = (new Date).getDate();
+    var monthF = ('00' + month).slice(-2);
+    var dayF = ('00' + day).slice(-2);
+    this.date = year+'-'+monthF+'-'+dayF;
+
+    this.dataMatching()
   }
-  date =''
+  // background
   backImg = ''
   background(){
     var back = document.getElementsByClassName('background')[0] as HTMLElement;
@@ -27,15 +33,59 @@ export class ProgramComponent implements OnInit {
       back.style.backgroundImage = 'url(../../../assets/photo/noimg.png)';
     }
   }
+  nd = new Date();
+  year = this.nd.getFullYear();
+  month = this.nd.getMonth()+1;
+  day = this.nd.getDate();
+  date=''
+  // 날짜 이동
   dateprev(){
-    var nd = new Date();
-    var year = nd.getFullYear();
-    var month = nd.getMonth()+1;
-    var date = nd.getDate();
-    
+    this.day = this.day - 1
+    if(this.day == 0){
+      this.month = this.month - 1;
+      if(this.month == 0){
+        this.year = this.year - 1
+        this.month = 12
+      }
+      this.day = new Date(this.year , this.month , 0).getDate();
+    }
+    var monthF = ('00' + this.month).slice(-2);
+    var dayF = ('00' + this.day).slice(-2);
+    this.date = this.year+'-'+monthF+'-'+dayF;
+
+    var noProgram = document.getElementsByClassName('noProgram')[0] as HTMLElement;
+        noProgram.style.display = 'block';
+    this.dataMatching()
   }
   datenext(){
-
+    this.day = this.day + 1
+    var lastDay = new Date(this.year , this.month , 0).getDate();
+    if(this.day == lastDay + 1){
+      this.month = this.month + 1;
+      if(this.month == 13){
+        this.year = this.year + 1
+        this.month = 1
+      }
+      this.day = 1
+    }
+    var monthF = ('00' + this.month).slice(-2);
+    var dayF = ('00' + this.day).slice(-2);
+    this.date = this.year+'-'+monthF+'-'+dayF;
+    
+    var noProgram = document.getElementsByClassName('noProgram')[0] as HTMLElement;
+        noProgram.style.display = 'block';
+    this.dataMatching()
   }
-  
+  // 데이터 매칭
+  rows = new Array;
+  dataMatching() {
+    this.rows = new Array;
+    for(var i =0; i<this.program.length; i++){
+      if(this.program[i].date == this.date){
+        this.rows.push(this.program[i])
+        var noProgram = document.getElementsByClassName('noProgram')[0] as HTMLElement;
+        noProgram.style.display = 'none';
+      }
+    }
+  }
 }
