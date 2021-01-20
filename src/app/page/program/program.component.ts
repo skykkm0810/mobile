13b7aed5542464c42328a2044a5ced51a37bfdbe
skykkm0810 <skykkm0810@naver.com>
2021-01-20 10:24:09 +0900

    program update

--- src/app/page/program/program.component.ts
+++ src/app/page/program/program.component.ts
@@ -6,11 +6,36 @@ import { PROGRAM} from '../../interface/interface';
   styleUrls: ['./program.component.css']
 })
 export class ProgramComponent implements OnInit {
-
+  program = PROGRAM
   constructor() { }
   ngOnInit(): void {
-    console.log(PROGRAM)
+    this.background()
+    var year = (new Date).getFullYear();
+    var month = (new Date).getMonth() + 1;
+    var date = (new Date).getDate();
   }
-  
+  date =''
+  backImg = ''
+  background(){
+    var back = document.getElementsByClassName('background')[0] as HTMLElement;
+    back.style.height = back.clientWidth + 'px';
+    back.style.background = 'no-repeat center center / contain'
+    if(this.backImg !== ''){
+      back.style.backgroundImage = 'url('+ this.backImg +')'
+    }
+    else {
+      back.style.backgroundImage = 'url(../../../assets/photo/noimg.png)';
+    }
+  }
+  dateprev(){
+    var nd = new Date();
+    var year = nd.getFullYear();
+    var month = nd.getMonth()+1;
+    var date = nd.getDate();
+    
+  }
+  datenext(){
+
+  }
   
 }
